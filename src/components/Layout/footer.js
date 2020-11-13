39c747aee1660d297e8648e4c499783b9dd51f14
d_yusef <d.yusefns@gmail.com>
2020-11-13 15:34:43 +0700

    dynamic footer name

--- src/components/Layout/footer.js
+++ src/components/Layout/footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment'
 
 export default class Footer extends Component {
   render() {
@@ -10,7 +11,7 @@ export default class Footer extends Component {
               <small><span className="badge-info">CAUTION</span> Trading crypto assets is a high risk activity. Crypto asset prices are highly volatile, where prices can change significantly from time to time. Please use extra judgment in making decisions to buy or sell crypto assets. Kahve is only responsible for the initial investment if the active balance is the same as your investment amount. All crypto asset trading decisions are independent decisions by the user.</small>
             </div>
             <div className="col-6 offset-3">
-              <p className="text-center">KahveBit <a href="/" className="external">2020</a></p>
+              <p className="text-center">&copy;&nbsp;{localStorage.getItem('site_title')} <a href="/" className="external">{moment().format('YYYY')}</a></p>
               {/* Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)*/}
             </div>
           </div>
